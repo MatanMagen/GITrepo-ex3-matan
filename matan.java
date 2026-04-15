f816a1d34f0b6ffae0b03e3fdf6194732217410f
matanmagen <matanmagen2002@gmail.com>
2026-04-15 20:33:25 +0300

    Update matan.java print message

--- matan.java
+++ matan.java
@@ -1,7 +1,7 @@
 public class matan{
     public static void main(String[] args) {
         for (int i=1;i<=10; i++) {
-            System.out.println("My name is Matan ..."+i);
+            System.out.println("Matan Magen rocks DevOps #"+i);
         }
     }
 }
